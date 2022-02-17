d496e10afe71cc1b2990e701475cab2fb06b8485
Nimna <e1846608@bit.mrt.ac.lk>
2022-02-17 20:18:59 +0530

    used Json to display posts

--- SmaPetz/posts/urls.py
+++ SmaPetz/posts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import home_view
+from .views import home_view, post_view_json
 
 app_name = 'posts'
 
 urlpatterns = [
     path('', home_view, name='home-view'),
+
+    #json
+    path('posts-json/', post_view_json, name='posts-view-json'),
 ]
--- SmaPetz/posts/views.py
+++ SmaPetz/posts/views.py
@@ -1,11 +1,12 @@
-from multiprocessing import context
+import django
 from django.shortcuts import render
 from .models import Post
+from django.http import JsonResponse
+from django.core import serializers
 
 # Create your views here.
 
 def home_view(request):
-
     #query set to grab all the posts
     qs = Post.objects.all()
     context = {
@@ -13,3 +14,8 @@ def home_view(request):
         'qs': qs
     }
     return render(request, 'posts/main.html', context)
+
+def post_view_json(request):
+    qs = Post.objects.all()
+    data = serializers.serialize('json', qs)
+    return JsonResponse({'data':data})
