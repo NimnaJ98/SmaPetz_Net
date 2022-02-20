7396dbd166798309ae3d04d40a17333578b2c601
Nimna <e1846608@bit.mrt.ac.lk>
2022-02-20 09:50:12 +0530

    edited pet types in the mdoel

--- SmaPetz/profiles/admin.py
+++ SmaPetz/profiles/admin.py
@@ -7,6 +7,19 @@ from .models import Profile, Pet, Veterinarian, Store, Pet_Lover
 @admin.register(Pet)
 class FonteAdmin(admin.ModelAdmin):
      readonly_fields = ['usertype1']
+     
+     if Pet.pet_type == "FISH":
+          readonly_fields = ['reptile_type','bird_type','amphibian_type','mammal_type']
+     elif Pet.pet_type == "REPTILE":
+          readonly_fields = ['fish_type','bird_type','amphibian_type','mammal_type']
+     elif Pet.pet_type == "BIRD":
+          readonly_fields = ['fish_type','reptile_type','amphibian_type','mammal_type']
+     elif Pet.pet_type == "AMPHIBIAN":
+          readonly_fields = ['fish_type','reptile_type','bird_type','mammal_type']
+     elif Pet.pet_type == "MAMMAL":
+          readonly_fields = ['fish_type','reptile_type','bird_type','amphibian_type']
+     
+     
 
 @admin.register(Veterinarian)
 class FonteAdmin(admin.ModelAdmin):
--- SmaPetz/profiles/models.py
+++ SmaPetz/profiles/models.py
@@ -21,17 +21,75 @@ class Profile(models.Model):
 class Pet(Profile):
     
     class petTypes(models.TextChoices):
-        CAT = "CAT", "Cat"
-        DOG = "DOG", "Dog"
         FISH = "FISH", "Fish"
-        HAMSTER = "HAMSTER", "Hamster"
-        RABBIT = "RABBIT", "Rabbit"
-        MOUSE = "MOUSE", "Mouse"
-        TURTLE = "TURTLE", "Turtle"
         REPTILE = "REPTILE", "Reptile"
         BIRD = "BIRD", "Bird"
         AMPHIBIAN = "AMPHIBIAN" ,'Amphibian'
         MAMMAL = "MAMMAL" ,'Mammal'
+        OTHER = "OTHER" , 'Other'
+
+    class fishTypes(models.TextChoices):
+        BETTA = "BETTA", "Betta"
+        GOLDFISH = "GOLDFISH", "Goldfish"
+        ANGELFISH = "ANGELFISH", "Angelfish"
+        GUPPIES = "GUPPIES" ,'Guppies'
+        NEONTETRAS = "NEONTETRAS" ,'Neon Tetras'
+        ZEBRADANIOS = "ZEBRADANIOS" ,'Zebra Danios'
+        MOLLIES = "MOLLIES" ,'Mollies'
+        CATFISH = "CATFISH" ,'Catfish'
+        CHERRYBARB = "CHERRYBARB" ,'Cherry Barb'
+        PLATY = "PLATY" ,' Platy'
+        SWORDTAIL = "SWORDTAIL" ,' Swordtail'
+        OTHER = "OTHER" , 'Other'
+
+    class reptileTypes(models.TextChoices):
+        BEARDEDDRAGON = "BEARDEDDRAGON", "Bearded Dragon"
+        GECKOS = "GECKOS", "Leopard Gecko"
+        TURTLES = "TURTLES", "Turtle"
+        TORTOISE = "TORTOISE" ,'Tortoise'
+        CORNSNAKES = "CORNSNAKES" ,'Corn Snake'
+        ZEBRADANIOS = "ZEBRADANIOS" ,'Ball Python'
+        CRESTEDGECKO = "CRESTEDGECKO" ,'Crested Gecko'
+        SKINK = "SKINK" ,'Blue-Tongued Skink'
+        GREENIGUANA = "GREENIGUANA" ,'Green Iguana'
+        ANOLE = "ANOLE" ,' Anole'
+        SNAKE = "SNAKE" ,'Snake'
+        WATERDRAGON = "WATERDRAGON" ,'Water Dragon'
+        OTHER = "OTHER" , 'Other'
+
+    class birdTypes(models.TextChoices):
+        PARROT = "PARROT", "Parrot"
+        COCKTIEL = "COCKTIEL", "Cockatiel"
+        FINCH = "FINCH", "Finch"
+        COCKATOO = "COCKATOO" ,'Cockatoo'
+        CANARY = "CANARY" ,'Canary'
+        LOVEBIRD = "LOVEBIRD" ,'Lovebird'
+        PARAKEET = "PARAKEET" ,'Parakeet'
+        DOVE = "DOVE" ,'Dove'
+        CONURES = "CONURES" ,'Conures'
+        MACAW = "MACAW" ,'Macaw'
+        OTHER = "OTHER" , 'Other'
+
+    class amphibianTypes(models.TextChoices):
+        TREEFROG = "TREEFROG", "Tree Frog"
+        SALMANDER = "SALMANDER", "Salamander"
+        FINCH = "AXOLOTL", "Axolotl"
+        PACEMANFROG = "PACEMANFROG" ,'Pacman Frog'
+        OFBT = "OFBT" ,'Oriental Fire-Bellied Toad'
+        EASTERNNEWT = "EASTERNNEWT" ,'Eastern Newt'
+        OTHER = "OTHER" , 'Other'
+
+    class mammalTypes(models.TextChoices):
+        GERBIL = "GERBIL", "Gerbil"
+        DOG = "DOG", "Dog"
+        CAT = "CAT", "Cat"
+        HAMSTER = "HAMSTER" ,'Hamster'
+        RABBIT = "RABBIT" ,'Rabbit'
+        RAT = "RAT" ,'Rat'
+        PIG = "PIG" ,'Guinea Pig'
+        FERRET = "FERRET" ,'Ferret'
+        HEDGEHOG = "HEDGEHOG" ,'Hedgehog'
+        FFOXES = "FFOXES" ,' Fennec Foxes'
         OTHER = "OTHER" , 'Other'
 
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='pet')
@@ -39,6 +97,13 @@ class Pet(Profile):
     following = models.ManyToManyField(User, related_name='pet_following', blank=True)
     avatar = models.ImageField(upload_to='avatars', default='pet_avatar.png')
     pet_type = models.CharField(max_length=50, choices=petTypes.choices, blank=True)
+
+    fish_type = models.CharField(max_length=50, choices=fishTypes.choices, blank=True)
+    reptile_type = models.CharField(max_length=50, choices=reptileTypes.choices, blank=True)
+    bird_type = models.CharField(max_length=50, choices=birdTypes.choices, blank=True)
+    amphibian_type = models.CharField(max_length=50, choices=amphibianTypes.choices, blank=True)
+    mammal_type = models.CharField(max_length=50, choices=mammalTypes.choices, blank=True)
+    
     breed = models.TextField(max_length=50, blank=True)
 
     def __str__(self):
