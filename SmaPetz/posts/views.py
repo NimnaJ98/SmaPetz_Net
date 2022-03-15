0c1cc0602f8b4bfad6f61704baa24445f84f4f6f
Nimna <e1846608@bit.mrt.ac.lk>
2022-03-15 22:20:31 +0530

    added navbar

--- SmaPetz/posts/views.py
+++ SmaPetz/posts/views.py
@@ -8,10 +8,9 @@ from django.core import serializers
 
 def home_view(request):
     #query set to grab all the posts
-    qs = Post.objects.all()
     context = {
         'hello': 'hello world',
-        'qs': qs
+        
     }
     return render(request, 'posts/main.html', context)
 
