76da87f105fd04866d95c89bc337ba19a7770d15
Nimna <e1846608@bit.mrt.ac.lk>
2022-03-17 10:57:19 +0530

    modified profiles view

--- SmaPetz/profiles/views.py
+++ SmaPetz/profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from matplotlib.style import context
 from .models import Profile, Pet, Pet_Lover, Store, Veterinarian
 from users.models import User
 from django.views.generic import TemplateView, View
@@ -9,18 +10,34 @@ from django.http import JsonResponse
 def profile_test_view(request):
     if request.user.type == "PET":
         profile = Pet.objects.get(user=request.user)
+
+        context = {
+            'profile': profile
+        }
         return render(request, 'profiles/pet_profile.html', {'profile':profile})
     
     elif request.user.type == "VET":
         profile = Veterinarian.objects.get(user=request.user)
+
+        context = {
+            'profile': profile
+        }
         return render(request, 'profiles/vet_profile.html', {'profile':profile})
 
     elif request.user.type == "STORE":
         profile = Store.objects.get(user=request.user)
+
+        context = {
+            'profile': profile
+        }
         return render(request, 'profiles/store_profile.html', {'profile':profile})
 
     elif request.user.type == "PET_LOVER":
         profile = Pet_Lover.objects.get(user=request.user)
+
+        context = {
+            'profile': profile
+        }
         return render(request, 'profiles/petLover_profile.html', {'profile':profile})
 
 
