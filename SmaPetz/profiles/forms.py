43a1fea248da7bd178f1c469f23cc600d5ef3a6d
Nimna <e1846608@bit.mrt.ac.lk>
2022-03-20 20:31:05 +0530

    styled the update modal 1

--- SmaPetz/profiles/forms.py
+++ SmaPetz/profiles/forms.py
@@ -5,3 +5,8 @@ class PetModelForm(forms.ModelForm):
     class Meta:
         model = Pet
         fields = {'avatar', 'bio', 'address', 'number','pet_type', 'breed', 'fish_type', 'reptile_type', 'bird_type', 'amphibian_type', 'mammal_type'}
+        widgets = {
+          'bio': forms.Textarea(attrs={'rows':2}),
+          'address': forms.Textarea(attrs={'rows':3}),
+          'breed': forms.Textarea(attrs={'rows':1}),
+        }
