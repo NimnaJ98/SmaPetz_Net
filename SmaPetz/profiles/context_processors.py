49bd648851c435f3a9b9eb9b9f96f7e0eced8f09
Nimna <e1846608@bit.mrt.ac.lk>
2022-04-23 18:18:51 +0530

    created context processors to get the profile picture to the navbar

---
+++ SmaPetz/profiles/context_processors.py
@@ -0,0 +1,15 @@
+from .models import Profile, FriendRequest
+
+def profile_pic(request):
+    if request.user.is_authenticated:
+        profile_obj = Profile.objects.get(user=request.user)
+        pic = profile_obj.profilePic
+        return {'picture':pic}
+    return {}
+
+def invitations_no(request):
+    if request.user.is_authenticated:
+        profile_obj = Profile.objects.get(user=request.user)
+        qs_count = FriendRequest.objects.received_requests(profile_obj).count()
+        return {'requests_no':qs_count}
+    return {}
