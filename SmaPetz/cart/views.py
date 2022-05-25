7900feb324328498bb36716ca87d4fd27d0d39a4
Nimna <e1846608@bit.mrt.ac.lk>
2022-05-25 21:05:18 +0530

    created remove from cart functionality

--- SmaPetz/cart/views.py
+++ SmaPetz/cart/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .cart import Cart
 
 # Create your views here.
 
 def cart_detail(request):
+    cart = Cart(request)
+    remove_from_cart = request.GET.get('remove_from_cart', '')
+    if remove_from_cart:
+        cart.remove(remove_from_cart)
+        return redirect('cart:cart')
+        
     return render(request, 'cart/cart.html')
